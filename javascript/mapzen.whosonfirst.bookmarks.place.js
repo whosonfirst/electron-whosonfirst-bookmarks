7b4c07662a1576fe617c02ed49d31424429fb2d0
thisisaaroland <aaron@mapzen.com>
2017-06-10 23:06:48 -0700

    start working on controls

--- javascript/mapzen.whosonfirst.bookmarks.place.js
+++ javascript/mapzen.whosonfirst.bookmarks.place.js
@@ -57,14 +57,42 @@
 			map.setAttribute("data-geom-longitude", pl["geom:longitude"]);
 			map.setAttribute("data-wofid", pl["wof:id"]);
 
+			var select = document.createElement("select");
+			select.setAttribute("id", "desire");
+
+			var desires = {
+				0: "i was there",
+				1: "i want to go there",
+				2: "again again",
+				3: "again",
+				4: "again maybe",
+				5: "again never",
+				6: "meh"
+			};
+
+			for (var id in desires){
+
+				var option = document.createElement("option");
+				option.setAttribute("value", id);
+				option.appendChild(document.createTextNode(desires[id]));
+
+				console.log(option);
+				select.appendChild(option);
+			}
+			
 			var button = document.createElement("button");
 			button.setAttribute("class", "btn btn-primary");
 			button.appendChild(document.createTextNode("Save"));
+
+			var controls = document.createElement("div");
+			controls.setAttribute("id", "controls");
+			controls.appendChild(select);
+			controls.appendChild(button);
 			
 			var wrapper = document.createElement("div");
 			wrapper.appendChild(h2);
 			wrapper.appendChild(map);
-			wrapper.appendChild(button);
+			wrapper.appendChild(controls);
 			
 			canvas.draw(wrapper);
 
